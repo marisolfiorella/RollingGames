21af23c12e2729549ef2d0fcc49168654dfa0757
Marcelo Frias <frias.marcelo@gmail.com>
2021-03-23 20:59:43 -0300

    cambio juegoClass de funcion a clase

--- js/juegoClass.js
+++ js/juegoClass.js
@@ -1,12 +1,12 @@
 //CREAR UNA CLASE PARA LOS JUEGOS
 
-export function Juego {
+export class Juego {
     constructor(codigo, nombre, numSerie, categoria, descripcion, imagen) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.numSerie = numSerie;
         this.categoria = categoria;
-        this.descripcion = desccripcion;
+        this.descripcion = descripcion;
         this.imagen = imagen;
     }
 }
