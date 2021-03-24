86d9cde99b3741e6bd1bdfca72180a26de735347
Marcelo Frias <frias.marcelo@gmail.com>
2021-03-24 01:53:23 -0300

    arregladas validaciones y envío de mail formulario contacto, arreglos errores marcados por prettier, arreglos de errores addEventListener

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,83 +1,88 @@
 function campoRequerido(nombres) {
-    if (nombres.value.trim() === "" || nombres.value.length >= 30) {
-        nombres.className = "form-control is-invalid";
-        return false;
-    } else {
-        nombres.className = "form-control is-valid";
-        return true;
-    }
+  if (nombres.value.trim() === "" || nombres.value.length >= 30) {
+    nombres.className = "form-control is-invalid";
+    return false;
+  } else {
+    nombres.className = "form-control is-valid";
+    return true;
+  }
 }
 
 function validarPassword(pass) {
-    if (pass.value.length >= 8) {
-        pass.className = "form-control is-valid";
-        return true;
-    } else {
-        pass.className = "form-control is-invalid";
-        return false;
-    }
+  if (pass.value.length >= 8) {
+    pass.className = "form-control is-valid";
+    return true;
+  } else {
+    pass.className = "form-control is-invalid";
+    return false;
+  }
 }
 
 function validarRPassword(rpass) {
-    let p1 = document.getElementById("password").value;
-    let p2 = document.getElementById("rPassword").value;
-    if (p1 === p2 && p2 != "") {
-        rpass.className = "form-control is-valid";
-        return true;
-    } else {
-        rpass.className = "form-control is-invalid";
-        return false;
-    }
+  let p1 = document.getElementById("password").value;
+  let p2 = document.getElementById("rPassword").value;
+  if (p1 === p2 && p2 != "") {
+    rpass.className = "form-control is-valid";
+    return true;
+  } else {
+    rpass.className = "form-control is-invalid";
+    return false;
+  }
 }
 
 function validarEmail(email) {
-    let expresion = /\w+@\w+\.[a-z]{2,}$/;
-    if (email.value.trim() != "" && expresion.test(email.value)) {
-        email.className = "form-control is-valid";
-        return true;
-    } else {
-        email.className = "form-control is-invalid";
-        return false;
-    }
+  let expresion = /\w+@\w+\.[a-z]{2,}$/;
+  if (email.value.trim() != "" && expresion.test(email.value)) {
+    email.className = "form-control is-valid";
+    return true;
+  } else {
+    email.className = "form-control is-invalid";
+    return false;
+  }
 }
 
 function validarTelefono(telefono) {
-    if (telefono.value.trim() != "" && !isNaN(telefono.value)) {
-        telefono.className = "form-control is-valid";
-        return true;
-    } else {
-        telefono.className = "form-control is-invalid";
-        return false;
-    }
+  if (
+    telefono.value.trim() != "" &&
+    !isNaN(telefono.value) &&
+    telefono.value <= 999999999999999
+  ) {
+    telefono.className = "form-control is-valid";
+    return true;
+  } else {
+    telefono.className = "form-control is-invalid";
+    return false;
+  }
 }
 
 function validarEdad(inputedad) {
-    if (
-        inputedad.value >= 12 &&
-        inputedad.value <= 121 &&
-        !isNaN(inputedad.value)
-    ) {
-        inputedad.className = "form-control is-valid";
-        return true;
-    } else {
-        inputedad.className = "form-control is-invalid";
-        return false;
-    }
+  if (
+    inputedad.value >= 12 &&
+    inputedad.value <= 121 &&
+    !isNaN(inputedad.value)
+  ) {
+    inputedad.className = "form-control is-valid";
+    return true;
+  } else {
+    inputedad.className = "form-control is-invalid";
+    return false;
+  }
 }
 
 let checkTerminos = document.getElementById("terminos");
-checkTerminos.addEventListener("change", function() {
+function checkBox() {
+  checkTerminos.addEventListener("change", function () {
     validarTerminos();
-});
-
+  });
+}
 function validarTerminos() {
-    if (checkTerminos.checked) {
-        checkTerminos.className = "form-check-input is-valid";
-        return true;
-    } else {
-        checkTerminos.className = "form-check-input is-invalid";
-        return false;
-    }
+  if (checkTerminos.checked) {
+    checkTerminos.className = "form-check-input is-valid";
+    return true;
+  } else {
+    checkTerminos.className = "form-check-input is-invalid";
+    return false;
+  }
 }
 
 function validarRegistro(event) {
@@ -97,91 +102,131 @@ function validarRegistro(event) {
   } else {
     Swal.fire({
       title: "Error",
-      text: "Debe completar correctament el formulario",
+      text: "Debe completar correctamente el formulario",
       icon: "error",
-      confirmButtonColor: "#3085d6",
+      confirmButtonColor: "#181717",
       confirmButtonText: "Aceptar",
     });
   }
 }
 
 function enviarEmail() {
-    emailjs
-        .send("service_zrczdis", "template_xcs5oks", {
-            from_name: document.getElementById("nombre").value,
-            to_name: "Administrador",
-            from_user: document.getElementById("usuario").value,
-            from_password: document.getElementById("password").value,
-            from_email: document.getElementById("email").value,
-            from_phone: document.getElementById("telefono").value,
-            from_age: document.getElementById("edad").value,
-        })
-        .then(
-            function(response) {
-                // esta funcion se ejecuta cuando se cumple la promesa
-                console.log(response);
-                Swal.fire({
-                    title: "¡Gracias!",
-                    text: "Usuario Registrado Correctamente",
-                    icon: "success",
-                    confirmButtonColor: "#3085d6",
-                    confirmButtonText: "Entendido!",
-                });
+  emailjs
+    .send("service_zrczdis", "template_xcs5oks", {
+      from_name: document.getElementById("nombre").value,
+      to_name: "Administrador",
+      from_user: document.getElementById("usuario").value,
+      from_password: document.getElementById("password").value,
+      from_email: document.getElementById("email").value,
+      from_phone: document.getElementById("telefono").value,
+      from_age: document.getElementById("edad").value,
+    })
+    .then(
+      function (response) {
+        console.log(response);
+        Swal.fire({
+          title: "¡Gracias!",
+          text: "Usuario Registrado Correctamente",
+          icon: "success",
+          confirmButtonColor: "#181717",
+          confirmButtonText: "Entendido!",
+        });
 
-                document.getElementById("formRegistro").reset();
-                document.getElementById("usuario");
-                usuario.className = "form-control";
-                document.getElementById("password");
-                password.className = "form-control";
-                document.getElementById("rPasssword");
-                rPassword.className = "form-control";
-                document.getElementById("nombre");
-                nombre.className = "form-control";
-                document.getElementById("email");
-                email.className = "form-control";
-                document.getElementById("telefono");
-                telefono.className = "form-control";
-                document.getElementById("edad");
-                edad.className = "form-control";
-                document.getElementById("terminos");
-                terminos.className = "form-check-input";
-            },
-            function(error) {
-                // esta funcion se ejecuta cuando dio error la promesa
-                console.log(error);
-                alert("No se pudo enviar el mail");
-            }
-        );
+        document.getElementById("formRegistro").reset();
+        document.getElementById("usuario");
+        usuario.className = "form-control";
+        document.getElementById("password");
+        password.className = "form-control";
+        document.getElementById("rPasssword");
+        rPassword.className = "form-control";
+        document.getElementById("nombre");
+        nombre.className = "form-control";
+        document.getElementById("email");
+        email.className = "form-control";
+        document.getElementById("telefono");
+        telefono.className = "form-control";
+        document.getElementById("edad");
+        edad.className = "form-control";
+        document.getElementById("terminos");
+        terminos.className = "form-check-input";
+      },
+      function (error) {
+        console.log(error);
+        alert("No se pudo enviar el mail");
+      }
+    );
 }
 
 //Validaciones adicionales para Contacto
-/* function validarConsulta(consulta) {
-    if (consulta.value.trim() === "" || consulta.value.length <= 10) {
-        consulta.className = "form-control is-invalid bg-secondary";
-        return false;
-    } else {
-        consulta.className = "form-control is-valid bg-secondary";
-        return true;
-    }
+function validarConsulta(consulta) {
+  if (
+    consulta.value.trim() === "" ||
+    consulta.value.length <= 10 ||
+    consulta.value.length >= 500
+  ) {
+    consulta.className = "form-control is-invalid";
+    return false;
+  } else {
+    consulta.className = "form-control is-valid";
+    return true;
+  }
 }
 
 function validarContacto(event) {
-    event.preventDefault();
-    console.log("desde validar contacto");
-    if (
-        campoRequerido(document.getElementById("nombre")) &&
-        validarEmail(document.getElementById("email")) &&
-        validarConsulta(document.getElementById('consulta'))
-    ) {
-        enviarEmailContacto();
-    } else {
-        alert("Debe corregir los datos de contacto");
-    }
+  event.preventDefault();
+  console.log("desde validar contacto");
+  if (
+    campoRequerido(document.getElementById("nombre")) &&
+    validarEmail(document.getElementById("email")) &&
+    validarConsulta(document.getElementById("consulta"))
+  ) {
+    enviarEmailContacto();
+  } else {
+    Swal.fire({
+      title: "Error",
+      text: "Debe completar correctamente el formulario",
+      icon: "error",
+      confirmButtonColor: "#181717",
+      confirmButtonText: "Aceptar",
+    });
+  }
 }
 
 function enviarEmailContacto() {
+  emailjs
+    .send("service_zrczdis", "template_upouiwm", {
+      from_name: document.getElementById("nombre").value,
+      to_name: "Administrador",
+      from_email: document.getElementById("email").value,
+      message: document.getElementById("consulta").value,
+    })
+    .then(
+      function (response) {
+        console.log(response);
+        Swal.fire({
+          title: "¡Consulta enviada correctamente!",
+          text: "Gracias por comunicarte con nosotros",
+          icon: "success",
+          confirmButtonColor: "#181717",
+          confirmButtonText: "Aceptar",
+        });
 
+        document.getElementById("formContacto").reset();
+        document.getElementById("nombre");
+        nombre.className = "form-control";
+        document.getElementById("email");
+        email.className = "form-control";
+        document.getElementById("consulta");
+        consulta.className = "form-control";
+      },
+      function (error) {
+        console.log(error);
+        alert("No se pudo enviar el mail");
+      }
+    );
 }
+
+/*
 
 
 // VALIDACION LOGIN
